e4250c693c31763d60e32dee1b20adaa33593d34
mbond <mbond@59f74c5a-d414-4ad8-883b-508a4601e97c>
2011-06-01 17:19:28 +0000

    Added Pretty Print, changed format of stack.

--- engine/engineAPI/3.0/modules/errorHandling/errorHandle.php
+++ engine/engineAPI/3.0/modules/errorHandling/errorHandle.php
@@ -2,6 +2,10 @@
 
 class errorHandle {
 
+	const ERROR   = "error";
+	const SUCCESS = "success";
+	const WARNING = "warning";
+
 	private $engine     = null;
 	
 	// Defines the element that gets used when a message output is returned. 
@@ -56,6 +60,81 @@ class errorHandle {
 		return($output);
 	}
 
+	public static function prettyPrint($type="all") {
+		
+		if (!(isset($this) && get_class($this) == __CLASS__)) {
+			$engine = EngineAPI::singleton();
+		}
+		else {
+			$engine = $this->engine;
+		}
+		
+		$output = '<ul class="errorPrettyPrint">';
+		
+		if ($type == "all") {
+			
+			if (!isset($engine->errorStack['all']) || !is_array($engine->errorStack['all'])) {
+				return(FALSE);
+			}
+			
+			foreach ($engine->errorStack['all'] as $V) {
+				
+				switch ($V['type']) {
+					case errorHandle::ERROR:
+						$class = "errorMessage";
+						break;
+					case errorHandle::SUCCESS:
+						$class = "successMessage";
+						break;
+					case errorHandle::WARNING:
+						$class = "warningMessage";
+						break;
+					default:
+						break;
+				}
+				
+				$output .= "<li>";
+				$output .= '<span class="'.$class.'">';
+				$output .= $V['message'];
+				$output .= "</span>";
+				$output .= "</li>";
+			}
+		}
+		else {
+			
+			if (!isset($engine->errorStack[$type]) || !is_array($engine->errorStack[$type])) {
+				return(FALSE);
+			}
+			
+			switch ($type) {
+				case errorHandle::ERROR:
+					$class = "errorMessage";
+					break;
+				case errorHandle::SUCCESS:
+					$class = "successMessage";
+					break;
+				case errorHandle::WARNING:
+					$class = "warningMessage";
+					break;
+				default:
+					break;
+			}
+			
+			foreach ($engine->errorStack[$type] as $V) {
+				
+				$output .= "<li>";
+				$output .= '<span class="'.$class.'">';
+				$output .= $V;
+				$output .= "</span>";
+				$output .= "</li>";
+			}
+		}
+		
+		$output .= '</ul>';
+		
+		return($output);
+	}
+
 	private function errorStack($type,$message) {
 		
 		$engine = NULL;
@@ -66,8 +145,20 @@ class errorHandle {
 		else {
 			$engine = $this->engine;
 		}
+		
+		if (!isset($engine->errorStack[$type])) {
+			$engine->errorStack[$type] = array();
+		}
+		if (!isset($engine->errorStack["all"])) {
+			$engine->errorStack["all"] = array();
+		}
+		
 		$engine->errorStack[$type][] = $message;
-		$engine->errorStack["all"][] = $message;
+		
+		$temp = array();
+		$temp['message'] = $message;
+		$temp['type']    = $type;
+		$engine->errorStack["all"][] = $temp;
 		
 		return;
 	}
