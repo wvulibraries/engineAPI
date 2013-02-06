f7883823a1d006ebdbbd816d21e667902d30ab7a
David Gersting <david.gersting@mail.wvu.edu>
2013-02-06 15:35:36 -0500

    Added PHPdoc blocks

--- engine/engineAPI/3.1/helperFunctions/wrappers.depricated.php
+++ engine/engineAPI/3.1/helperFunctions/wrappers.depricated.php
@@ -1,44 +1,88 @@
 <?php
 
+/**
+ * Determine if an email is internal
+ *
+ * @depreciated
+ * @see email::internalEmailAddr()
+ * @param $email
+ * @return bool
+ */
 function internalEmail($email) {
 	return(email::internalEmailAddr($email));
 }
 
+/**
+ * [Web Helper] Generate error message
+ * @depreciated
+ * @see errorHandle::errorMsg()
+ * @param $message
+ * @return string
+ */
 function webHelper_errorMsg($message) {
 	return(errorHandle::errorMsg($message));
 }
 
+/**
+ * [Web Helper] Generate success message
+ * @depreciated
+ * @see errorHandle::successMsg()
+ * @param $message
+ * @return string
+ */
 function webHelper_successMsg($message) {
 	return(errorHandle::successMsg($message));
 }
 
+/**
+ * Determine if browser is mobile
+ *
+ * @depreciated
+ * @see mobileBrowsers::isMobileBrowser()
+ * @return null
+ */
 function engine_isMobileBrowser() {
 	return(mobileBrowsers::isMobileBrowser());
 }
 
-// Template Date
-// $attPairs['time'] == unix time, Option, current time if not provided
-// $attPairs['format'] for date();
-// function tempDate($attPairs) {
-// 
-// 	if (isset($attPairs['time'])) {
-// 		return(date($attPairs['format'],$attPairs['time']));
-// 	}
-// 
-// 	return(date($attPairs['format']));
-// 	
-// }
-
+/**
+ * Generate HTML date drop-down
+ *
+ * @depreciated
+ * @see date->dateDropDown()
+ * @param $attPairs
+ * @param null $engine
+ * @return string
+ */
 function dateDropDown($attPairs,$engine=null) {
 	$date = new date();
 	return($date->dateDropDown($attPairs,$engine=null));
 }
 
+/**
+ * Generate HTML time drop-down
+ *
+ * @depreciated
+ * @see date->timeDropDown()
+ * @param $attPairs
+ * @param null $engine
+ * @return string
+ */
 function timeDropDown($attPairs,$engine=null) {
 	$date = new date();
 	return($date->timeDropDown($attPairs,$engine=null));
 }
 
+/**
+ * Generate a file list?
+ *
+ * @depreciated
+ * @see fileList
+ * @param $dir
+ * @param $template
+ * @param $engine
+ * @return mixed
+ */
 function filelist($dir,$template,$engine) {
 	
 	global $engineVars;
@@ -52,11 +96,27 @@ function filelist($dir,$template,$engine) {
 	return($output);
 }
 
-function printENV($engine=NULL) {
+/**
+ * Prints the current environment
+ *
+ * @depreciated
+ * @see debug::printENV()
+ */
+function printENV(){
 	debug::printENV();
 	return;
 }
 
+/**
+ * Generate print_r() output that's output-buffer safe
+ *
+ * @depreciated
+ * @see debug::obsafe_print_r()
+ * @param $var
+ * @param bool $return
+ * @param int $level
+ * @return string
+ */
 function obsafe_print_r($var, $return = TRUE, $level = 0) {
 	$return = debug::obsafe_print_r($var, $return, $level);
 	if (!is_empty($return)) {
