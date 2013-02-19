b3469c85288c743e7cd088b139dd6652eef54cef
David Gersting <david.gersting@mail.wvu.edu>
2013-02-19 15:22:50 -0500

    Added phpDoc blocks and a bit of code cleanup

--- engine/engineAPI/3.1/modules/eapi_session/eapi_session.php
+++ engine/engineAPI/3.1/modules/eapi_session/eapi_session.php
@@ -1,25 +1,24 @@
 <?php
-
+/**
+ * Legacy EngineAPI session stuff
+ * @deprecated
+ */
 class eapi_session {
-	
-	//Template Stuff
-	// private $pattern = "/\{eapi_include\s+(.+?)\}/";
-	// private $function = "eapi_includes::templateMatches";
-	
-	function __construct() {		
-		// EngineAPI::defTempPatterns($this->pattern,$this->function,$this);
-		// EngineAPI::defTempPatterns("/\{engine name=\"include\"\s+(.+?)\}/",$this->function,$this);
-		
+	function __construct() {
 		EngineAPI::defTempPatterns("/\{csrf\s+(.+?)\}/","eapi_session::csrf",$this);
-		
 		EngineAPI::defTempPatterns("/\{engine name=\"csrf\"\s*(.*?)\}/","eapi_session::csrf",$this);
 		EngineAPI::defTempPatterns("/\{engine name=\"insertCSRF\"\s*(.*?)\}/","eapi_session::csrf",$this);
-		
 		EngineAPI::defTempPatterns("/\{engine name=\"csrfGet\"\s*(.*?)\}/","eapi_session::csrfGet",$this);
-		
 		EngineAPI::defTempPatterns("/\{engine name=\"session\"\s+(.+?)\}/","eapi_session::sessionGet",$this);
 	}
-	
+
+	/**
+	 * Get something from the session
+	 *
+	 * @deprecated
+	 * @param $matches
+	 * @return mixed|null
+	 */
 	public static function sessionGet($matches) {
 		$engine        = EngineAPI::singleton();
 		$eapi_function = $engine->retTempObj("eapi_function");
@@ -28,28 +27,47 @@ class eapi_session {
 		$output = sessionGet($attPairs['var']);
 		return($output);
 	}
-	
+
+	/**
+	 * Insert the csrf
+	 *
+	 * @deprecated
+	 * @param $matches
+	 * @return mixed|null|string
+	 */
 	public static function csrf($matches) {
-		
 		$engine        = EngineAPI::singleton();
 		$eapi_session  = $engine->retTempObj("eapi_session");
 		$attPairs      = attPairs($matches[1]);
 		
-		if (isset($attPairs['insert']) && strtolower($attPairs['insert']) != "post") {
+		if(isset($attPairs['insert']) && strtolower($attPairs['insert']) != "post"){
 			$output = sessionInsertCSRF(FALSE);
-		}
-		else {
+		}else{
 			$output = sessionInsertCSRF();
 		}
 		
 		return($output);
 	}
-	
+
+	/**
+	 * Get the csrf
+	 *
+	 * @deprecated
+	 * @param $matches
+	 * @return mixed|null|string
+	 */
 	public static function csrfGet($matches) {
 		$output = sessionInsertCSRF(FALSE);
 		return($output);
 	}
-	
+
+	/**
+	 * Template handler
+	 *
+	 * @deprecated
+	 * @param $matches
+	 * @return bool|string
+	 */
 	public static function templateMatches($matches) {
 		$engine        = EngineAPI::singleton();
 		$eapi_function = $engine->retTempObj("eapi_function");
@@ -63,7 +81,6 @@ class eapi_session {
 		
 		return($output);
 	}
-	
 }
 
 ?>
