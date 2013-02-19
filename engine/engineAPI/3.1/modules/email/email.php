d258dc669c253c46dc23cd97ff5f389fe2271c90
David Gersting <david.gersting@mail.wvu.edu>
2013-02-19 15:26:58 -0500

    Added phpDoc blocks

--- engine/engineAPI/3.1/modules/email/email.php
+++ engine/engineAPI/3.1/modules/email/email.php
@@ -1,15 +1,30 @@
 <?php
-
+/**
+ * Helper class for email
+ */
 class email {
-
+	/**
+	 * Validate an email address
+	 *
+	 * @see validate::emailAddr()
+	 * @param $email
+	 * @param bool $internal Passed through to validate::emailAddr()
+	 * @return bool
+	 */
 	public static function validate($email,$internal=FALSE) {
 		return(validate::emailAddr($email,$internal));
 	}
 
+	/**
+	 * Validate internal email
+	 *
+	 * @see validate::internalEmailAddr()
+	 * @param $email
+	 * @return bool
+	 */
 	public static function internalEmailAddr($email) {
 		return(validate::internalEmailAddr($email));
 	}
-
 }
 
 ?>
