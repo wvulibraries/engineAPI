1345bd8c6e525b46fa56e5e2346261ae8edf9adb
David Gersting <david.gersting@mail.wvu.edu>
2013-02-21 11:22:26 -0500

    Added phpDoc blocks

--- engine/engineAPI/3.1/modules/phpself/phpself.php
+++ engine/engineAPI/3.1/modules/phpself/phpself.php
@@ -1,36 +1,36 @@
 <?php
 
 class phpself {
-	
-	private $engine = NULL;
 	public $pattern  = "/\{phpself\s+(.+?)\}/";
 	public $function = "phpself::templateMatches";
-	
+
+	/**
+	 * Class constructor
+	 */
 	function __construct() {
 		$engine = EngineAPI::singleton();
 		$engine->defTempPattern($this->pattern,$this->function,$this);
 	}
-	
+
+	/**
+	 * Engine tag handler
+	 *
+	 * @param $matches
+	 * @return string
+	 */
 	public static function templateMatches($matches) {
-		
 		$engine   = EngineAPI::singleton();
-
 		$obj      = $engine->retTempObj("phpself");
-
 		$attPairs = attPairs($matches[1]);
-
-		$phpself = $_SERVER['PHP_SELF'];
+		$phpself  = $_SERVER['PHP_SELF'];
 
 		if (strtolower($attPairs['query']) == "true") {
 			$qs = preg_replace('/&amp;/','&',$_SERVER['QUERY_STRING']);
-
 			$phpself .= "?".$qs;
 		}
 
 		return($phpself);
-		
 	}
-	
 }
 
 ?>
