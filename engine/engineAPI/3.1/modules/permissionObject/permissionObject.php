202a4bdbd2b2d12300cd37b1696bd87459704e5d
David Gersting <david.gersting@mail.wvu.edu>
2013-02-21 10:40:01 -0500

    Added phpDoc blocks and a little code cleanup

--- engine/engineAPI/3.1/modules/permissionObject/permissionObject.php
+++ engine/engineAPI/3.1/modules/permissionObject/permissionObject.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Legacy permissions object
+ * This system uses the bitwise system where each permission is assigned a unique int
+ */
 class permissionObject {
 
 	/*
@@ -12,91 +16,81 @@ class permissionObject {
 	private $engine    = NULL;
 	private $permsList = array();
 
+	/**
+	 * Class constructor
+	 *
+	 * @todo Remove deprecated usage of webHelper_errorMsg()
+	 * @param string $table
+	 *        The database table the permissions are stored in
+	 * @param engineDB $database
+	 *        The database object to interact with
+	 */
 	function __construct($table,$database=NULL) {
 		$this->table    = $table;
 		$this->engine   = EngineAPI::singleton();
 		$this->database = ($database instanceof engineDB) ? $database : $this->engine->openDB;
 
-		$sql = sprintf("SELECT value FROM `%s` ORDER BY value + 0",
-			$this->database->escape($this->table)
-			);
+		$sql = sprintf("SELECT value FROM `%s` ORDER BY value + 0", $this->database->escape($this->table));
 		$sqlResult = $this->database->query($sql);
 
-		if (!$sqlResult['result']) {
-			return webHelper_errorMsg("Error pulling permissions from table in Constructor");
-		}
+		if(!$sqlResult['result']) return webHelper_errorMsg("Error pulling permissions from table in Constructor");
 
-		while ($row = mysql_fetch_array($sqlResult['result'],  MYSQL_NUM)) {
+		while($row = mysql_fetch_array($sqlResult['result'],  MYSQL_NUM)){
 			$this->permsList[] = (string)$row[0];
 		}
 	}
 
-	function __destruct() {
-	}
-
+	/**
+	 * Insert new function into permission table
+	 * In this contact, function is the 'thing' that is to protect (eg create_user)
+	 *
+	 * @param $function
+	 * @return bool
+	 */
 	public function insert($function) {
 
 		// Check for Duplicates
-		$sql = sprintf("SELECT * FROM `%s` WHERE name='%s'",
-			$this->engine->dbTables($this->table),
-			$this->database->escape($function)
-			);
+		$sql = sprintf("SELECT * FROM `%s` WHERE name='%s'", $this->engine->dbTables($this->table), $this->database->escape($function));
 		$sqlResult = $this->database->query($sql);
 
-		if (!$sqlResult['result']) {
-			return(FALSE);
-		}
-
-		if (mysql_num_rows($sqlResult['result']) > 0) {
-			return(FALSE);
-		}
+		if(!$sqlResult['result']) return(FALSE);
+		if(mysql_num_rows($sqlResult['result']) > 0) return(FALSE);
 
 		// Make sure that only valid Strings are entered
 		$return = preg_match("/^[a-zA-Z0-9\-\_]+$/",$this->database->escape($function));
-		if ($return == 0) {
-			return(FALSE);
-		}
+		if($return == 0) return(FALSE);
 
 		$value = $this->generateNextNumber();
-		if (empty($value)) {
-			return(FALSE);
-		}
+		if(empty($value)) return(FALSE);
 
 		$sql = sprintf("INSERT INTO `%s` (name,value) VALUES('%s','%s')",
 			$this->engine->dbTables($this->table),
-			$this->database->escape($function), 
-			$this->database->escape($value)
-			);
+			$this->database->escape($function),
+			$this->database->escape($value));
 		$sqlResult = $this->database->query($sql);
 
-		if (!$sqlResult['result']) {
-			//print webHelper_errorMsg($sql.":". $sqlResult['error']);
-			return(FALSE);
-		}
-
+		if(!$sqlResult['result']) return(FALSE);
 		return(TRUE);
 	}
 
-	// $number = assigned Number
-	// $permission = number of permissions
-	// determine $number contains $permission.
+	/**
+	 * Check a given permission
+	 *
+	 * @param string $number
+	 *        The user's assigned number
+	 * @param string $permission
+	 *        The number for the permission you are checking
+	 *
+	 * @return bool
+	 */
 	public function checkPermissions($number,$permission) {
+		if(bccomp($permission,$number) == 1) return(FALSE);
 
-		if (bccomp($permission,$number) == 1) {
-			return(FALSE);
-		}
+		for($I = count($this->permsList) - 1;$I >= 0; $I--){
+			if(bccomp($permission,$number) == 1) return(FALSE);
 
-		for ($I = count($this->permsList) - 1;$I >= 0; $I--) {
-
-			if (bccomp($permission,$number) == 1) {
-				return(FALSE);
-			}
-
-			if (bccomp($number,$this->permsList[$I]) == 1 || bccomp($number,$this->permsList[$I]) == 0) {
-				if (bccomp($this->permsList[$I],$permission) == 0) {
-					return(TRUE);
-				}
-
+			if(bccomp($number,$this->permsList[$I]) == 1 || bccomp($number,$this->permsList[$I]) == 0){
+				if(bccomp($this->permsList[$I],$permission) == 0) return(TRUE);
 				$number = bcsub($number,$this->permsList[$I]);
 				continue;
 			}
@@ -105,17 +99,18 @@ class permissionObject {
 		return(FALSE);	
 	}
 
-	// $permissions is the security number that the user currently has
+	/**
+	 * Build the 'wall of checkboxes' for the possible permissions
+	 *
+	 * @param string $permissions
+	 *        The security number that the user currently has
+	 * @return string
+	 */
 	public function buildFormChecklist($permissions) {
-
-		$sql = sprintf("SELECT * FROM `%s`",
-			$this->database->escape($this->engine->dbTables("permissions"))
-			);
+		$sql = sprintf("SELECT * FROM `%s`", $this->database->escape($this->engine->dbTables("permissions")));
 		$sqlResult = $this->database->query($sql);
 
-		if (!$sqlResult['result']) {
-			return webHelper_errorMsg("Error pulling permissions from table.");
-		}
+		if(!$sqlResult['result']) return webHelper_errorMsg("Error pulling permissions from table.");
 
 		// This should be in a template instead of hard-coded HTML
 		$output = '<ul class="perissionsCheckBoxList">';
@@ -135,11 +130,12 @@ class permissionObject {
 		return($output);
 	}
 
+	/**
+	 * Retrieves the next available permission number
+	 * @return string
+	 */
 	private function generateNextNumber() {
-
-		$sql = sprintf("SELECT value FROM `%s`",
-			$this->database->escape($this->engine->dbTables($this->table))
-			);
+		$sql = sprintf("SELECT value FROM `%s`", $this->database->escape($this->engine->dbTables($this->table)));
 		$sqlResult = $this->database->query($sql);
 
 		$count = "0";
@@ -149,10 +145,7 @@ class permissionObject {
 			}
 		}
 
-		if(empty($count)) {
-			return(1);
-		}
-
+		if(empty($count)) return(1);
 		return(bcmul($count, 2));
 	}
 }
