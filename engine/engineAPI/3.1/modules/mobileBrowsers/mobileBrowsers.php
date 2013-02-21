f4d00e5fd7fbf6b79871d46da466c262ca36e179
David Gersting <david.gersting@mail.wvu.edu>
2013-02-21 09:55:02 -0500

    Added phpDoc blocks and did a little code cleanup

--- engine/engineAPI/3.1/modules/mobileBrowsers/mobileBrowsers.php
+++ engine/engineAPI/3.1/modules/mobileBrowsers/mobileBrowsers.php
@@ -1,69 +1,61 @@
 <?php
 
 class mobileBrowsers {
-
 	private static $uaResult = NULL;
 
 	private static function construct() {
-
-		if (!isnull(self::$uaResult) && self::$uaResult !== FALSE) {
-			return(TRUE);
-		}
-
+		if(!isnull(self::$uaResult) && self::$uaResult !== FALSE) return(TRUE);
 		require_once(dirname(__FILE__)."/tobie-ua-parser-php/UAParser.php");
 		self::$uaResult = UA::parse();
 
-		if (self::$uaResult === FALSE) {
-			return(FALSE);
-		}
-
+		if(self::$uaResult === FALSE) return(FALSE);
 		return(TRUE);
 	}
 
+	/**
+	 * Returns TRUE if the UserAgent is a mobile browser
+	 * @return bool
+	 */
 	public static function isMobileBrowser() {
-
 		self::construct();
-
-		if (isnull(self::$uaResult) || self::$uaResult === FALSE) {
-			return(NULL);
-		}
-
+		if(isnull(self::$uaResult) || self::$uaResult === FALSE) return(NULL);
 		return self::$uaResult->isMobile;
 	}
 
+	/**
+	 * Returns TRUE if the UserAgent is a tablet browser
+	 * @return bool
+	 */
 	public static function isTabletBrowser() {
-
 		self::construct();
-
-		if (isnull(self::$uaResult) || self::$uaResult === FALSE) {
-			return(NULL);
-		}
-
+		if(isnull(self::$uaResult) || self::$uaResult === FALSE) return(NULL);
 		return self::$uaResult->isTablet;
 	}
 
+	/**
+	 * Returns what kind of devide the UserAgent is
+	 * If valid UA, returns 'tablet', 'mobile', or 'desktop'
+	 *
+	 * @return mixed
+	 */
 	public static function deviceIs() {
-
 		self::construct();
-
-		if (self::$uaResult === FALSE || isnull(self::$uaResult)) {
-			return(self::$uaResult);
+		if (self::$uaResult === FALSE || isnull(self::$uaResult)) return(self::$uaResult);
+		switch(true){
+			case self::$uaResult->isMobile:
+				return "tablet";
+			case self::$uaResult->isTablet:
+				return "mobile";
+			default:
+				return "desktop";
 		}
-
-		$mobile = self::$uaResult->isMobile;
-		$tablet = self::$uaResult->isTablet;
-
-		if ($tablet === TRUE) {
-			return("tablet");
-		}
-		if ($mobile === TRUE) {
-			return("mobile");
-		}
-
-		return("desktop");
-
 	}
 
+	/**
+	 * Returns debug text for display
+	 *
+	 * @return string
+	 */
 	public static function prettyPrint() {
 
 		self::construct();
