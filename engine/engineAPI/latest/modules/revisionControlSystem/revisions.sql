6f6365b71ea35a46f531f8f0b548f039b20866f3
MichaelRBond <mbond@the-forgotten.org>
2013-03-06 09:51:56 -0500

    Updated structure of revisions table to be innodb

--- engine/engineAPI/latest/modules/revisionControlSystem/revisions.sql
+++ engine/engineAPI/latest/modules/revisionControlSystem/revisions.sql
@@ -31,7 +31,7 @@ CREATE TABLE `revisions` (
   `digitalObjects` blob,
   `relatedData` text,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
