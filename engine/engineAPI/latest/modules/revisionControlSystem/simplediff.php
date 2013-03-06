ec639c5af3afbc1d0b5d2ca747ab7c2f42b3ed63
David Gersting <david.gersting@mail.wvu.edu>
2013-03-06 17:07:12 -0500

    Added missing phpDoc blocks

--- engine/engineAPI/latest/modules/revisionControlSystem/simplediff.php
+++ engine/engineAPI/latest/modules/revisionControlSystem/simplediff.php
@@ -1,23 +1,31 @@
 <?php
+/**
+ * Paul's Simple Diff Algorithm v 0.1
+ * @author  Paul Butler
+ * @license May be used and distributed under the zlib/libpng license
+ * @link    http://www.paulbutler.org
+ * @package simpleDiff
+ *
+ * This code is intended for learning purposes; it was written with short
+ * code taking priority over performance. It could be used in a practical
+ * application, but there are a few ways it could be optimized.
+ *
+ * Given two arrays, the function diff will return an array of the changes.
+ * I won't describe the format of the array, but it will be obvious
+ * if you use print_r() on the result of a diff on some test data.
+ *
+ * htmlDiff is a wrapper for the diff command, it takes two strings and
+ * returns the differences in HTML. The tags used are <ins> and <del>,
+ * which can easily be styled with CSS.
+ */
 
-/*
-	Paul's Simple Diff Algorithm v 0.1
-	(C) Paul Butler 2007 <http://www.paulbutler.org/>
-	May be used and distributed under the zlib/libpng license.
-	
-	This code is intended for learning purposes; it was written with short
-	code taking priority over performance. It could be used in a practical
-	application, but there are a few ways it could be optimized.
-	
-	Given two arrays, the function diff will return an array of the changes.
-	I won't describe the format of the array, but it will be obvious
-	if you use print_r() on the result of a diff on some test data.
-	
-	htmlDiff is a wrapper for the diff command, it takes two strings and
-	returns the differences in HTML. The tags used are <ins> and <del>,
-	which can easily be styled with CSS.  
-*/
-
+/**
+ * diff()
+ *
+ * @param $old
+ * @param $new
+ * @return array
+ */
 function diff($old, $new){
     $matrix = array();
 	$maxlen = 0;
@@ -40,6 +48,13 @@ function diff($old, $new){
 		diff(array_slice($old, $omax + $maxlen), array_slice($new, $nmax + $maxlen)));
 }
 
+/**
+ * htmlDiff()
+ *
+ * @param $old
+ * @param $new
+ * @return string
+ */
 function htmlDiff($old, $new){
 	$ret = '';
 	$diff = diff(explode(' ', $old), explode(' ', $new));
