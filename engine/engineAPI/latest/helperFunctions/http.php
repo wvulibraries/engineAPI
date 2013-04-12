7fe08d0fb65c3eaaf8a7477925de41d8f5f1d83e
MichaelRBond <mbond@the-forgotten.org>
2013-04-12 07:58:10 -0400

    cleanup to conform to local style documentation

--- engine/engineAPI/latest/helperFunctions/http.php
+++ engine/engineAPI/latest/helperFunctions/http.php
@@ -66,8 +66,7 @@ class http
      * @param string $url
      * @param int $statusCode
      */
-    public static function redirect($url, $statusCode=307)
-    {
+    public static function redirect($url, $statusCode=307) {
         $statusCode = (int)$statusCode;
         $validCodes = array(201,301,304,305,307);
         if(!in_array($statusCode, $validCodes)){
@@ -86,8 +85,7 @@ class http
      * @param int $statusCode
      * @param boolean $replace
      */
-    public static function sendStatus($statusCode, $replace=true)
-    {
+    public static function sendStatus($statusCode, $replace=true) {
         // Clean params
         $statusCode = (int)$statusCode;
         $replace = (bool)$replace;
@@ -161,8 +159,7 @@ class http
      * @param mixed $data
      * @return string
      */
-    public static function compressData($data)
-    {
+    public static function compressData($data) {
         return strtr(base64_encode(addslashes(gzcompress(serialize($data),9))), '+/=', '-_,');
     }
 
@@ -172,8 +169,7 @@ class http
      * @param string $string
      * @return mixed
      */
-    public static function decompressData($string)
-    {
+    public static function decompressData($string) {
         return unserialize(gzuncompress(stripslashes(base64_decode(strtr($string, '-_,', '+/=')))));
     }
 }
