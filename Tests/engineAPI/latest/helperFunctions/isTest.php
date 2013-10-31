58dbb16b6c0c8d0547b6408f03a483bbe818ac0e
Michael Bond <mbond@th-forgotten.org>
2013-10-31 09:53:51 -0400

    corrected typo

--- Tests/engineAPI/latest/helperFunctions/isTest.php
+++ Tests/engineAPI/latest/helperFunctions/isTest.php
@@ -34,17 +34,17 @@ class isTest extends PHPUnit_Framework_TestCase {
 
 	public function test_isint_IntegerReturnsTrue() {
 		$this->assertTrue(isint(1234));
-		$this->markIncomplete('Need to stub validate class.');
+		$this->markTestIncomplete('Need to stub validate class.');
 	}
 
 	public function test_isint_FloatReturnsFalse() {
 		$this->assertFalse(isint(1234.56));
-		$this->markIncomplete('Need to stub validate class.');
+		$this->markTestIncomplete('Need to stub validate class.');
 	}
 
 	public function test_isint_StringReturnsFalse() {
 		$this->assertFalse(isint('foo'));
-		$this->markIncomplete('Need to stub validate class.');
+		$this->markTestIncomplete('Need to stub validate class.');
 	}
 
 	public function test_isnull_NullReturnsTrue() {
