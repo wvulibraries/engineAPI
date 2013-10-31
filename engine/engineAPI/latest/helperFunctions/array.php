58c1dbdd79d545db81db7ede9953418b8e936ed1
Michael Bond <mbond@th-forgotten.org>
2013-10-31 13:45:21 -0400

    added whitespace for readability

--- engine/engineAPI/latest/helperFunctions/array.php
+++ engine/engineAPI/latest/helperFunctions/array.php
@@ -212,14 +212,15 @@ function buildECMSArray($array) {
  */
 function simpleXMLToArray(SimpleXMLElement $xml,$attributesKey=null,$childrenKey=null,$valueKey=null){ 
 
-    if($childrenKey && !is_string($childrenKey)){$childrenKey = '@children';} 
-    if($attributesKey && !is_string($attributesKey)){$attributesKey = '@attributes';} 
-    if($valueKey && !is_string($valueKey)){$valueKey = '@values';} 
-
+    if ($childrenKey   && !is_string($childrenKey))   $childrenKey = '@children';
+    if ($attributesKey && !is_string($attributesKey)) $attributesKey = '@attributes';
+    if ($valueKey      && !is_string($valueKey))      $valueKey = '@values';
+    
     $return = array(); 
-    $name = $xml->getName(); 
+    $name   = $xml->getName(); 
     $_value = trim((string)$xml); 
-    if(!strlen($_value)){$_value = null;}; 
+
+    if (!strlen($_value)) $_value = null; 
 
     if($_value!==null){ 
         if($valueKey){$return[$valueKey] = $_value;} 
@@ -227,38 +228,54 @@ function simpleXMLToArray(SimpleXMLElement $xml,$attributesKey=null,$childrenKey
     } 
 
     $children = array(); 
-    $first = true; 
-    foreach($xml->children() as $elementName => $child){ 
+    $first    = true; 
+    foreach($xml->children() as $elementName => $child) { 
+
         $value = simpleXMLToArray($child,$attributesKey, $childrenKey,$valueKey); 
-        if(isset($children[$elementName])){ 
-            if(is_array($children[$elementName])){ 
-                if($first){ 
+        
+        if(isset($children[$elementName])) { 
+            if(is_array($children[$elementName])) { 
+               if($first) { 
                     $temp = $children[$elementName]; 
                     unset($children[$elementName]); 
                     $children[$elementName][] = $temp; 
                     $first=false; 
                 } 
                 $children[$elementName][] = $value; 
-            }else{ 
+            }
+            else{ 
                 $children[$elementName] = array($children[$elementName],$value); 
             } 
         } 
         else{ 
             $children[$elementName] = $value; 
         } 
+
     } 
-    if($children){ 
-        if($childrenKey){$return[$childrenKey] = $children;} 
-        else{$return = array_merge($return,$children);} 
+    if($children) { 
+
+        if ($childrenKey) {
+            $return[$childrenKey] = $children;
+        }
+        else {
+            $return = array_merge($return,$children);
+        } 
+        
     } 
 
     $attributes = array(); 
-    foreach($xml->attributes() as $name=>$value){ 
+    
+    foreach($xml->attributes() as $name=>$value) { 
         $attributes[$name] = trim($value); 
     } 
-    if($attributes){ 
-        if($attributesKey){$return[$attributesKey] = $attributes;} 
-        else{$return = array_merge($return, $attributes);} 
+
+    if($attributes) { 
+        if ($attributesKey) {
+            $return[$attributesKey] = $attributes;
+        } 
+        else {
+            $return = array_merge($return, $attributes);
+        } 
     } 
 
     return $return; 
