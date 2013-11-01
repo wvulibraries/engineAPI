1cedba839ffb74d0be0265c611967afcf4375d96
Michael Bond <mbond@th-forgotten.org>
2013-11-01 14:15:06 -0400

    moved files to new "replacements" module

--- engine/engineAPI/latest/modules/eapi_function/onLoad.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// we are loading this automatically because it takes the place of older engineAPI 
-// functionality and needs to be loaded to catch those instances where it is/was used.
-
-require_once("eapi_function.php");
-$eapif = new eapi_function();
-
-?>
--- engine/engineAPI/latest/modules/replacements/onLoad.php
+++ engine/engineAPI/latest/modules/replacements/onLoad.php
@@ -3,6 +3,9 @@
 // we are loading this automatically because it takes the place of older engineAPI 
 // functionality and needs to be loaded to catch those instances where it is/was used.
 
+require_once("eapi_function.php");
+$eapif = new eapi_function();
+
 require_once("eapi_includes.php");
 $eapii = new eapi_includes();
 
