280fa79ae856b7775fe1a6d3683ce9cc1e96d8e3
Michael Bond <mbond@th-forgotten.org>
2013-11-05 10:53:58 -0500

    initial commit of functions class. removes extension functions from engineAPI and puts them here.

---
+++ engine/engineAPI/latest/modules/functions/functions.php
@@ -0,0 +1,120 @@
+<?php
+
+class functions {
+	
+	private $functionExtensions= array();
+
+	/**
+	 * Set function extensions
+	 *
+	 * @param string|array $function
+	 * @param string|array $newFunction
+	 * @param string $stage identifier of when this callback will be called. 
+	 *                      Used by the calling function to allow the calling function to 
+	 *                      have multiple Extensions. 
+	 * @return bool
+	 */
+	public function setFunctionExtension($function,$newFunction,$stage="after") {
+
+		$class       = (is_array($function))?$function[0]:NULL;
+		$function    = (is_array($function))?$function[1]:$function;
+		$newClass    = (is_array($newFunction))?$newFunction[0]:NULL;
+		$newFunction = (is_array($newFunction))?$newFunction[1]:$newFunction;
+
+		// check if the function/method exists
+
+		if (isnull($newClass) && functionExists($newFunction) === FALSE) {
+			return FALSE;
+		}
+		else if (isnull($newClass) && $newFunction == "recurseInsert") {
+			// can't define the system recurseInsert as the function.
+			return FALSE;
+		}
+		else if (!isnull($newClass) && functionExists($newClass,$newFunction) === FALSE) {
+			return FALSE;
+		}
+
+		$functionIndex = $function.((isnull($class))?"":"::".$class);
+
+		$temp             = array();
+		$temp['class']    = $newClass;
+		$temp['function'] = $newFunction;
+
+		if (!isset($this->functionExtensions[$functionIndex][$stage])) {
+			$this->functionExtensions[$functionIndex][$stage] = array();
+		}
+
+		$this->functionExtensions[$functionIndex][$stage][] = $temp;
+
+		return TRUE;
+	}
+
+	/**
+	 * Get function extensions
+	 *
+	 * @param string $function
+	 * @param string|null $class
+	 * @return array|bool
+	 */
+	public function getFunctionExtension($function,$class=NULL) {
+		$functionIndex = $function.((isnull($class))?"":"::".$class);
+		if (array_key_exists($functionIndex,$this->functionExtensions)) {
+			return $this->functionExtensions[$functionIndex];
+		}
+		return FALSE;
+	}
+
+	/**
+	 * Execute something...
+	 * @param string $function
+	 * @param string $params
+	 * @param string $stage identifier of when this callback will be called. 
+	 *                      Used by the calling function to allow the calling function to 
+	 *                      have multiple Extensions. 
+	 * @return bool
+	 */
+	public function execFunctionExtension($function,$params,$stage="after") {
+
+		if (!is_array($params)) {
+			return FALSE;
+		}
+
+		$class     = (is_array($function))?$function[0]:NULL;
+		$function  = (is_array($function))?$function[1]:$function;
+
+
+		$functions = $this->getFunctionExtension($function,$class);
+
+		if (!is_array($functions) || count($functions) < 1) {
+			return FALSE;
+		}
+
+		if (!array_key_exists($stage, $functions)) {
+			return FALSE;
+		}
+
+		$output = FALSE;
+
+
+
+		// return FALSE;
+		foreach($functions[$stage] as $I=>$function) {
+			if (array_key_exists('class',$function) && !isnull($function['class'])) {
+				$obj    = new $function['class'];
+				$output = $obj->$function['function']($params);
+			}
+			else {
+				$output = $function['function']($params);
+			}
+			if ($output !== FALSE) {
+				break;
+			}
+		}
+
+		return $output;
+
+	}
+
+}
+
+?>
