a02f6a759b08d459e40df994b584fe943e5cb97e
Michael Bond <mbond@th-forgotten.org>
2013-11-18 10:43:18 -0500

    load template

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,12 @@ $engine = EngineAPI::singleton();
 // Turns on verbose debugging in the apache error logs
 errorHandle::errorReporting(errorHandle::E_ALL);
 
+templates::load("distribution");
+templates::display("header");
 ?>
 
 <p>Hello World</p>
+
+<?php
+templates::display("footer");
+?>
