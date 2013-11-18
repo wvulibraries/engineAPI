87fa42902365e1515ca5aa1dd79a75132854b6e5
Michael Bond <mbond@th-forgotten.org>
2013-11-18 09:22:19 -0500

    initial vagrant setup

---
+++ bootstrap.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Base PRE Setup
+
+GITDIR="/tmp/git"
+ENGINEAPIHOME="/home/engineAPI"
+
+SERVERURL="/home/engineAPI"
+DOCUMENTROOT="public_html"
+
+yum -y install httpd httpd-devel httpd-manual httpd-tools
+yum -y install mysql-connector-java mysql-connector-odbc mysql-devel mysql-lib mysql-server
+yum -y install mod_auth_kerb mod_auth_mysql mod_authz_ldap mod_evasive mod_perl mod_security mod_ssl mod_wsgi 
+yum -y install php php-bcmath php-cli php-common php-gd php-ldap php-mbstring php-mcrypt php-mysql php-odbc php-pdo php-pear php-pear-Benchmark php-pecl-apc php-pecl-imagick php-pecl-memcache php-soap php-xml php-xmlrpc 
+yum -y install emacs emacs-common emacs-nox
+yum -y install git
+
+mv /etc/httpd/conf.d/mod_security.conf /etc/httpd/conf.d/mod_security.conf.bak
+/etc/init.d/httpd start
+
+mkdir -p $GITDIR
+cd $GITDIR
+git clone https://github.com/wvulibraries/engineAPITemplates.git
+git clone https://github.com/wvulibraries/engineAPI-Modules.git
+
+mkdir -p $SERVERURL/phpincludes/
+ln -s /vagrant/engine/ $SERVERURL/phpincludes/
+
+# Application Specific
+
+ln -s /vagrant/public_html $SERVERURL/$DOCUMENTROOT
+ln -s $SERVERURL/phpincludes/engine/engineAPI/latest $SERVERURL/phpincludes/engine/engineAPI/4.0
+
+rm -f /etc/php.ini
+rm -f /etc/httpd/conf/httpd.conf
+
+ln -s /vagrant/serverConfiguration/php.ini /etc/php.ini
+ln -s /vagrant/serverConfiguration/vagrant_httpd.conf /etc/httpd/conf/httpd.conf
+
+ln -s /tmp/git/engineAPI/engine/template/distribution/public_html/js $SERVERURL/$DOCUMENTROOT/javascript/distribution
+
+mkdir -p /vagrant/serverConfiguration/serverlogs
+touch /vagrant/serverConfiguration/serverlogs/error_log
+/etc/init.d/httpd restart
+
+# Base Post Setup
+
+## Setup the EngineAPI Database
+
+/etc/init.d/mysqld start
+mysql -u root < /vagrant/sql/vagrantSetup.sql
+mysql -u root EngineAPI < /vagrant/sql/EngineAPI.sql
