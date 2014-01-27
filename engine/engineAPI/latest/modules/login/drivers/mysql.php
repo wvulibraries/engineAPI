21aa46d52545c82043ebf0bc5e465c3b1add5531
David Gersting <dgersting@gmail.com>
2014-01-27 13:57:01 -0500

    Converted to new database module

--- engine/engineAPI/latest/modules/login/drivers/mysql.php
+++ engine/engineAPI/latest/modules/login/drivers/mysql.php
@@ -17,40 +17,24 @@ $loginFunctions['mysql'] = "mysqlLogin";
  * @return bool
  */
 function mysqlLogin($username,$password) {
-	
-	global $engineDB;
-	
-
-	$engine     = EngineAPI::singleton();
 	$enginevars = enginevars::getInstance();
+	$engineDB   = db::get('engineDB');
 
-	if (!$enginevars->is_set("mysqlAuthTable")) {
-		$enginevars->get("mysqlAuthTable","users");
+	if (!($engineDB instanceof dbDriver)) {
+		errorHandle::newError(__METHOD__."() No engineDB database driver to use!", errorHandle::DEBUG);
+		return FALSE;
 	}
-	
-	$engineDB = $engine->getPrivateVar("engineDB");
-	
-	$sql = sprintf("SELECT * FROM %s WHERE username='%s' AND password='%s'",
-		$engineDB->escape($enginevars->get("mysqlAuthTable")),
-		$engineDB->escape($username),
-		$engineDB->escape(md5($password))
-		);
-		
-	$engineDB->sanitize = FALSE;			
-	$sqlResult = $engineDB->query($sql);
-	
-	if (!$sqlResult['result'] || mysql_num_rows($sqlResult['result']) == 0) {
-		return(FALSE);
-	}
-	
+
+	$dbTable  = $engineDB->escape($enginevars->get("mysqlAuthTable", "users"));
+	$dbResult = $engineDB->query("SELECT * FROM `$dbTable` WHERE `username`='?' AND `password`='?'", array($username, md5($password)));
+	if (!$dbResult->rowCount()) return FALSE;
+
 	$_SESSION['groups']   = "";
 	$_SESSION['ou']       = "";
 	$_SESSION['username'] = $username;
 	$_SESSION['authType'] = "mysql";
 
-
-	return(TRUE);
-	
+	return TRUE;
 }
 
 ?>
