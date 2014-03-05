fda5c454b26a5d767c011c4f97665eceeac3a578
David Gersting <dgersting@gmail.com>
2014-03-05 15:52:19 -0500

    Added array_set() array_get() and array_unset() helper functions These functions let you manipulate arrays through dot notation (ie foo.bar.baz) Lifted from Laravel framework (MIT License)

--- engine/engineAPI/latest/helperFunctions/array.php
+++ engine/engineAPI/latest/helperFunctions/array.php
@@ -370,4 +370,88 @@ function array_peak($arr,$side='TOP'){
             return array_pop($arr2);
     }
 }
+
+/**
+ * Set an array item to a given value using "dot" notation.
+ *
+ * If no key is given to the method, the entire array will be replaced.
+ *
+ * @see https://github.com/laravel/framework/blob/master/src/Illuminate/Support/helpers.php#L403
+ * @param  array   $array
+ * @param  string  $key
+ * @param  mixed   $value
+ * @return array
+ */
+function array_set(&$array, $key, $value){
+	if (is_null($key)) return $array = $value;
+
+	$keys = explode('.', $key);
+
+	while (count($keys) > 1) {
+		$key = array_shift($keys);
+
+		// If the key doesn't exist at this depth, we will just create an empty array
+		// to hold the next value, allowing us to create the arrays to hold final
+		// values at the correct depth. Then we'll keep digging into the array.
+		if (!isset($array[$key]) || !is_array($array[$key])) {
+			$array[$key] = array();
+		}
+
+		$array =& $array[$key];
+	}
+
+	$array[array_shift($keys)] = $value;
+
+	return $array;
+}
+
+/**
+ * Get an item from an array using "dot" notation.
+ *
+ * @see https://github.com/laravel/framework/blob/master/src/Illuminate/Support/helpers.php#L300
+ * @param  array   $array
+ * @param  string  $key
+ * @param  mixed   $default
+ * @return mixed
+ */
+function array_get($array, $key, $default = NULL){
+	if (is_null($key)) return $array;
+
+	if (isset($array[$key])) return $array[$key];
+
+	foreach (explode('.', $key) as $segment) {
+		if (!is_array($array) || !array_key_exists($segment, $array)) {
+			return $default;
+		}
+
+		$array = $array[$segment];
+	}
+
+	return $array;
+}
+
+/**
+ * Remove an array item from a given array using "dot" notation.
+ *
+ * @see https://github.com/laravel/framework/blob/master/src/Illuminate/Support/helpers.php#L270
+ * @param  array   $array
+ * @param  string  $key
+ * @return void
+ */
+function array_unset(&$array, $key){
+	$keys = explode('.', $key);
+
+	while (count($keys) > 1) {
+		$key = array_shift($keys);
+
+		if (!isset($array[$key]) || !is_array($array[$key])) {
+			return;
+		}
+
+		$array =& $array[$key];
+	}
+
+	unset($array[array_shift($keys)]);
+}
+
 ?>
