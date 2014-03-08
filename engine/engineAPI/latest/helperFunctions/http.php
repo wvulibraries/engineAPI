d804f0168f6aa9dc8fcb7fc6d33871a7e45b06b4
David Gersting <dgersting@gmail.com>
2014-03-08 14:54:30 -0500

    Massive improvement to http::statusCode() for maintainability

--- engine/engineAPI/latest/helperFunctions/http.php
+++ engine/engineAPI/latest/helperFunctions/http.php
@@ -149,73 +149,39 @@ class http
      *
      * @param int $statusCode
      * @param boolean $replace
+	 * @return bool
      */
     public static function sendStatus($statusCode, $replace=true) {
-        // Clean params
-        $statusCode = (int)$statusCode;
-        $replace = (bool)$replace;
+		$statusCode = (int)$statusCode;
+		$replace    = (bool)$replace;
+		$validCodes = array(
+			101 => 'Switching Protocols',
+			300 => 'Multiple Choices',
+			301 => 'Moved Permanently',
+			302 => 'Found',
+			303 => 'See Other',
+			304 => 'Not Modified',
+			306 => 'Switch Proxy',
+			307 => 'Temporary Redirect',
+			308 => 'Permanent Redirect',
+			400 => 'Bad Request',
+			401 => 'Authorization Required',
+			403 => 'Forbidden',
+			404 => 'Not Found',
+			407 => 'Proxy Authentication Required',
+			426 => 'Upgrade Required',
+			500 => 'Internal Server Error',
+			503 => 'Service Temporarily Unavailable',
+		);
 
-        // Figure out what status we're sending
-        switch( (int)$statusCode ){
-            case 101:
-                // Informational - Switching Protocols
-                header("HTTP/1.1 101 Switching Protocols", $replace);
-                header("Status: 101 Switching Protocols", $replace);
-                break;
-            case 301:
-                // Redirect - Moved Permanently
-                header("HTTP/1.1 301 Moved Permanently", $replace);
-                header("Status: 301 Moved Permanently", $replace);
-                break;
-            case 307:
-                // Redirect - Temporary Redirect
-                header("HTTP/1.1 307 Temporary Redirect", $replace);
-                header("Status: 307 Temporary Redirect", $replace);
-                break;
-            case 400:
-                // Client Error - Bad Request
-                header("HTTP/1.1 400 Bad Request", $replace);
-                header("Status: 400 Bad Request", $replace);
-                break;
-            case 401:
-                // Client Error - Authorization Required
-                header("HTTP/1.1 401 Authorization Required", $replace);
-                header("Status: 401 Authorization Required", $replace);
-                break;
-            case 403:
-                // Client Error - Forbidden
-                header("HTTP/1.1 403 Forbidden", $replace);
-                header("Status: 403 Forbidden", $replace);
-                break;
-            case 404:
-                // Client Error - Not Found
-                header("HTTP/1.1 404 Not Found", $replace);
-                header("Status: 404 Not Found", $replace);
-                break;
-            case 407:
-                // Client Error - Proxy Authentication Required
-                header("HTTP/1.1 407 Proxy Authentication Required", $replace);
-                header("Status: 407 Proxy Authentication Required", $replace);
-                break;
-            case 426:
-                // Client Error - Upgrade Required
-                header("HTTP/1.1 426 Upgrade Required", $replace);
-                header("Status: 426 Upgrade Required", $replace);
-                break;
-            case 500:
-                // Server Error - Internal Server Error
-                header("HTTP/1.1 500 Internal Server Error", $replace);
-                header("Status: 500 Internal Server Error", $replace);
-                break;
-            case 503:
-                // Server Error - Service Temporarily Unavailable
-                header("HTTP/1.1 503 Service Temporarily Unavailable", $replace);
-                header("Status: 503 Service Temporarily Unavailable", $replace);
-                break;
-            default:
-                trigger_error(__METHOD__."() - Invalid HTTP status code", E_USER_NOTICE);
-                break;
-        }
+		if(isset($validCodes[$statusCode])){
+			header("HTTP/1.1 $statusCode ".$validCodes[$statusCode], $replace);
+			header("Status: $statusCode ".$validCodes[$statusCode], $replace);
+			return TRUE;
+		}else{
+			trigger_error(__METHOD__."() - Invalid HTTP status code '$statusCode'", E_USER_NOTICE);
+			return FALSE;
+		}
     }
 
     /**
