be9874cef7e68c7ab79ffe02189fe00577e056c3
Michael Bond <mbond@th-forgotten.org>
2014-03-24 08:33:52 -0400

    unit tests

--- Tests/engineAPI/latest/modules/time/timeTest.php
+++ Tests/engineAPI/latest/modules/time/timeTest.php
@@ -28,5 +28,45 @@ class validateTest extends PHPUnit_Framework_TestCase {
 		$this->assertFalse($this->time->toSeconds("1"));
 	}
 
+	public function test_convert_toSeconds() {
+
+		$this->assertEquals("48600",$this->time->convert("1:30pm"));
+		$this->assertEquals("48630",$this->time->convert("1:30:30pm"));
+		$this->assertEquals("48600",$this->time->convert("13:30"));
+		$this->assertEquals("48600",$this->time->convert("1330"));
+		$this->assertEquals("48630",$this->time->convert("13:30:30"));
+		$this->assertEquals("46800",$this->time->convert("1pm"));
+		$this->assertEquals("48630",$this->time->convert("1:30:30.5pm"));
+		$this->assertEquals("48630.5",$this->time->convert("1:30:30.5pm",TRUE));
+		$this->assertEquals("46800",$this->time->convert("1pm",TRUE));
+		$this->assertEquals("0",$this->time->convert("24:00",TRUE));
+		$this->assertEquals("0",$this->time->convert("12:00am",TRUE));
+
+		$this->assertFalse($this->time->convert("25:00"));
+		$this->assertFalse($this->time->convert("Foo"));
+		$this->assertFalse($this->time->convert("1"));
+
+	}
+
+	public function test_toTime() {
+
+		$this->assertEquals("1:30:00pm",$this->time->toTime(48600,TRUE));
+		$this->assertEquals("13:30:00",$this->time->toTime(48600,FALSE));
+		$this->assertEquals("13:30:30",$this->time->toTime(48630,FALSE));
+		$this->assertEquals("01:30",$this->time->toTime(48630,"h:i"));
+		$this->assertEquals("01:30PM",$this->time->toTime(48630,"h:iA"));
+
+	}
+
+	public function test_convert_toTime() {
+
+		$this->assertEquals("1:30:00pm",$this->time->convert(48600,TRUE));
+		$this->assertEquals("13:30:00",$this->time->convert(48600,FALSE));
+		$this->assertEquals("13:30:30",$this->time->convert(48630,FALSE));
+		$this->assertEquals("01:30",$this->time->convert(48630,"h:i"));
+		$this->assertEquals("01:30PM",$this->time->convert(48630,"h:iA"));
+		
+	}
+
 }
 ?>
