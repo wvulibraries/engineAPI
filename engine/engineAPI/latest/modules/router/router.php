fd7bf444aea6ba0cf5e8196b38a94c05ec543a99
Michael Bond <mbond@th-forgotten.org>
2014-04-07 14:51:52 -0400

    added some commenting for .htaccess

--- engine/engineAPI/latest/modules/router/router.php
+++ engine/engineAPI/latest/modules/router/router.php
@@ -1,5 +1,31 @@
 <?php
 
+/*
+
+router makes the assumption that there is a .htaccess in your application that looks
+like this, where 'myapp' is replaced with the document root (URI) of your application:
+
+<IfModule mod_rewrite.c>
+RewriteEngine On
+
+## recursively search parent dir
+# if index.php is not found then
+# forward to the parent directory of current URI
+RewriteCond %{DOCUMENT_ROOT}/myapp/$1$2/index.php !-f
+RewriteRule ^(.*?)([^/]+)/[^/]+/?$ /myapp/$1$2/ [L]
+
+# if current index.php is found in parent dir then load it
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{DOCUMENT_ROOT}/myapp/$1index.php -f
+RewriteRule ^(.*?)[^/]+/?$ /myapp/$1index.php [L]
+
+</IfModule>
+
+It recursively looks backwards for the closest index.php file and puts all the 
+'fake' URI information there. 
+
+ */
+
 class router {
 
 	private static $instance = NULL;
