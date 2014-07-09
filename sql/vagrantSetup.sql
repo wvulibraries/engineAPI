acd69b9eab373cccb615407c8bdf97a64e408c69
Scott Blake <Scott.Blake@mail.wvu.edu>
2014-07-09 12:42:30 -0400

    Further changes to conform to project standards

--- sql/vagrantSetup.sql
+++ sql/vagrantSetup.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS `EngineAPI`;
-CREATE DATABASE EngineAPI;
+CREATE DATABASE IF NOT EXISTS `EngineAPI`;
 
 CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON EngineAPI.* TO 'username'@'localhost';
+GRANT ALL PRIVILEGES ON `EngineAPI`.* TO 'username'@'localhost';
+USE `EngineAPI`;
