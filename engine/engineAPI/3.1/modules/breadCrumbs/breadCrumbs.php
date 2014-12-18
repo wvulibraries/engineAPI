c8b493d1c0704f7c500fa170dce17776c6c770c5
Michael Bond <mbond@the-forgotten.org>
2014-12-18 08:28:19 -0500

    Update breadCrumbs.php

--- engine/engineAPI/3.1/modules/breadCrumbs/breadCrumbs.php
+++ engine/engineAPI/3.1/modules/breadCrumbs/breadCrumbs.php
@@ -1,6 +1,7 @@
 <?php
 
 class breadCrumbs {
+
 	private $engine  = NULL;
 	public $pattern  = "/\{breadCrumbs\s+(.+?)\}/";
 	public $function = "breadCrumbs::templateMatches";
@@ -10,34 +11,21 @@ class breadCrumbs {
 		$this->engine->defTempPattern($this->pattern,$this->function,$this);
 	}
 
-	/**
-	 * Engine template tag callback
-	 * @param $matches
-	 * @return mixed
-	 */
 	public static function templateMatches($matches) {
 		$engine   = EngineAPI::singleton();
+
 		$obj      = $engine->retTempObj("breadCrumbs");
+
 		$attPairs = attPairs($matches[1]);
+
 		return($obj->breadCrumbs($attPairs));
+
 	}
 
-	/**
-	 * Generate HTML breadcrumbs
-	 *
-	 * @todo Fix use of deprecated use of webHelper_errorMsg()
-	 * @todo Remove use of deprecated global $engineVars
-	 * @param array $attPairs
-	 *        -titlecase  - Automatically convert cases of output to Title Case
-	 *        -ellipse    - Define the ellipse char to use
-	 *        -spacer     - Define the spacer char to use
-	 *        -type       - hierarchical or actual
-	 *        -displayNum - Limit number of crumbs to show
-	 *        -prefixNum  - Unknown
-	 * @return bool|string
-	 */
 	public function breadCrumbs($attPairs) {
+
 		$engine   = EngineAPI::singleton();
+
 		global $engineVars;
 
 		$callingFunction        = array("breadCrumbs","breadCrumbs");
@@ -45,10 +33,17 @@ class breadCrumbs {
 		$tempParams['attPairs'] = $attPairs;
 		$trail                  = $engine->execFunctionExtension($callingFunction,$tempParams,"before");
 
-		if($trail) return($trail);
+		if ($trail) {
+			return($trail);
+		}
 
 		/* setup initial variables */
-		$str2upper = (isset($attPairs['titlecase']) and str2bool($attPairs['titlecase']));
+		$str2upper = FALSE;
+		if (isset($attPairs['titlecase'])) {
+			if (strtoupper($attPairs['titlecase']) == "TRUE") {
+				$str2upper = TRUE;
+			}
+		}
 
 		$ellipse = (isset($engineVars['breadCrumbsEllipse']))?$engineVars['breadCrumbsEllipse']:" &#133; ";
 		$ellipse = (isset($attPairs['ellipse']))?$attPairs['ellipse']:$ellipse;
@@ -57,6 +52,7 @@ class breadCrumbs {
 		$spacer = (isset($attPairs['spacer']))?$attPairs['spacer']:$spacer;
 
 		$type   = (isset($attPairs['type']))?$attPairs['type']:"hierarchical";
+
 		$displayNum = (isset($engineVars['breadCrumbsDisplayNum']))?$engineVars['breadCrumbsDisplayNum']:0;
 		$displayNum = (isset($attPairs['displayNum']))?$attPairs['displayNum']:$displayNum;
 
@@ -74,9 +70,9 @@ class breadCrumbs {
 			$urlCount = count($url);
 			unset($url[--$urlCount]);
 
-			if ($displayNum) {
-				if ($displayNum < $urlCount) {
-					$start = $urlCount - $displayNum;
+			if (isset($attPairs['displayNum'])) {
+				if ($attPairs['displayNum'] < $urlCount) {
+					$start = $urlCount - $attPairs['displayNum'];
 				}
 				if (isset($attPairs['prefixNum'])) {
 					$prefix = $attPairs['prefixNum'];
@@ -104,12 +100,13 @@ class breadCrumbs {
 					}
 				}
 
-				if (file_exists($path ."/.breadCrumbs")) {
-					$lines = file($path ."/.breadCrumbs");
-					$trailArray[] = '<a href="'.$href.'" class="breadCrumbLink">'.(($str2upper === TRUE)?str2TitleCase($lines[0]):$lines[0]).'</a>';
+				$displayName = $this->displayName($path,$url[$I],$str2upper);
+
+				if ($I == $urlCount-1) {
+					$trailArray[] = $displayName;
 				}
 				else {
-					$trailArray[] = '<a href="'.$href.'" class="breadCrumbLink">'.(($str2upper === TRUE)?str2TitleCase($url[$I]):$url[$I]).'</a>';
+					$trailArray[] = sprintf('<a href="%s" class="breadCrumbLink">%s</a>',$href,$displayName);
 				}
 			}
 
@@ -123,6 +120,21 @@ class breadCrumbs {
 
 
 		return($trail);
+
+	}
+
+	private function displayName($path,$url,$str2upper) {
+
+		$displayName = "";
+		if (file_exists($path ."/.breadCrumbs")) {
+			$lines = file($path ."/.breadCrumbs");
+			$displayName = $lines[0];
+		}
+		else {
+			$displayName = $url;
+		}
+
+		return (($str2upper === TRUE)?str2TitleCase($displayName):$displayName);
 
 	}
 }
