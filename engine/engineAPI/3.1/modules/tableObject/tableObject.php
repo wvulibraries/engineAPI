30e6c9ef7480261442b06ca8515b054121a5ac15
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2024-04-24 17:32:04 -0400

    Update tableObject.php
    refactored tableObject class fixed the creator display showing array instead of the creator names.

--- engine/engineAPI/3.1/modules/tableObject/tableObject.php
+++ engine/engineAPI/3.1/modules/tableObject/tableObject.php
@@ -1,403 +1,255 @@
 <?php
+// Table Object
+// Version: 3.1
+// Refactored: 2024-04-24 by copilot and Tracy A. McCormick
 
 class tableObject {
 
-	/**
-	 * @var EngineAPI
-	 */
-	private $engine         = NULL;
-
-	/**
-	 * @todo This doesn't look used
-	 * @var int
-	 */
-	private $numColumns     = NULL;
-
-	/**
-	 * Array of column headers
-	 * @var string[]
-	 */
-	private $colHeaders     = NULL;
-
-	/**
-	 * Array of column footers
-	 * @var string[]
-	 */
-	private $colFooters = NULL;
-
-	/**
-	 * Denotes how and where the object gets its data from.
-	 *   - array: [Default] Data comes in as an array of arrays. Each index is a row of data for the table
-	 *   - mysql: A mysql query is provided. each row of data returned from the query is a row of data for the table
-	 * @var string
-	 */
+	private $engine;
 	private $tableType = "array";
+	public $class;
+	public $id;
+	public $classTD = false;
+	public $title;
+	public $summary;
+	public $summaryErrorMsg = true;
+	public $rowStriping = true;
+	public $numberRows = false;
+	public $sortable = false;
+	public $layout = false;
+	public $groupBy;
 
-	/**
-	 * CSS Class for the table
-	 * @var string
-	 */
-	public $class = NULL;
+	private $colHeaders = array();
+	private $colFooters = array();
+	private $data = array();
 
-	/**
-	 * CSS ID for the Table
-	 * If you have more than 1 table on a page, ID is highly recommended.
-	 * Many items will prefix id to other classes/IDs if it is available
-	 * @var string
-	 */
-	public $id = NULL;
-
-	/**
-	 * Prints the column number as a class on each TD in the Table
-	 * @var bool
-	 */
-	public $classTD = FALSE;
-
-	/**
-	 * HTML5 Title Attribute
-	 * @var string
-	 */
-	public $title = NULL;
-
-	/**
-	 * Table Summary
-	 * Required for accessibility - will print an error if not provided
-	 * @var null
-	 */
-	public $summary = NULL;
-
-	/**
-	 * If no summary is provided, print a reminder message
-	 * @var bool
-	 */
-	public $summaryErrorMsg = TRUE;
-
-	/**
-	 * Zebra stripe the edit table
-	 * @var bool
-	 */
-	public $rowStriping = TRUE;
-
-	/**
-	 * Rows are numbered on the far left
-	 * @var bool
-	 */
-	public $numberRows = FALSE;
-
-	/**
-	 * Edit Table is sortable by click on headers
-	 * @var bool
-	 */
-	public $sortable = FALSE;
-
-	/**
-	 * When true, add thead, tbody, and tfoot elements to the table
-	 * @var bool
-	 */
-	public $layout = FALSE;
-
-	/**
-	 * If groupby is not null, the array of rows will be examined and grouped by this column
-	 * this is the index number of the column, starting with 0
-	 * @var int
-	 */
-	public $groupBy = NULL;
-
-	/**
-	 * Class constructor
-	 *
-	 * @param null $type
-	 *   - array: [Default] Data comes in as an array of arrays. Each index is a row of data for the table
-	 *   - mysql: A mysql query is provided. each row of data returned from the query is a row of data for the table
-	 */
-	function __construct($type=NULL) {
-
+	public function __construct($type = null) {
 		$this->engine = EngineAPI::singleton();
-		
-		if (!isnull($type)) {
-			if ($type != "array" && $type != "mysql") {
-				return(FALSE);
-			}
+		if (!is_null($type) && in_array($type, array("array", "mysql"))) {
 			$this->tableType = $type;
 		}
-
 	}
-	
-	/**
-	 * Add column header(s)
-	 *
-	 * @param string|array $header
-	 *        Item(s) will be pushed onto the $colHeaders array
-	 * @return bool
-	 *         TRUE on success, FALSE otherwise.
-	 */
+
 	public function headers($header) {
-		
 		if (is_string($header)) {
-			if (isnull($this->colHeaders)) {
-				$this->colHeaders = array();
-			}
-			
 			$this->colHeaders[] = $header;
-			return(TRUE);
+			return true;
+		} elseif (is_array($header)) {
+			$this->colHeaders = array_merge($this->colHeaders, $header);
+			return true;
 		}
-		else if (is_array($header)) {
-			if (isnull($this->colHeaders)) {
-				$this->colHeaders = array();
-			}
-			
-			foreach ($header as $I=>$V) {
-				$this->colHeaders[] = $V;
-			}
-			return(TRUE);
-		}
-		
-		return(FALSE);
-		
-	}
-	
-	/**
-	 * Add column footer(s)
-	 *
-	 * @param string|array $footer
-	 *        Item(s) will be pushed onto the $colHeaders array
-	 * @return bool
-	 *         TRUE on success, FALSE otherwise.
-	 */
-	public function footers($footer) {
-		
-		if (is_string($footer)) {
-			if (isnull($this->colFooters)) {
-				$this->colFooters = array();
-			}
-			
-			$this->colFooters[] = $footer;
-			return(TRUE);
-		}
-		else if (is_array($footer)) {
-			if (isnull($this->colFooters)) {
-				$this->colFooters = array();
-			}
-			
-			foreach ($footer as $I=>$V) {
-				$this->colFooters[] = $V;
-			}
-			return(TRUE);
-		}
-		
-		return(FALSE);
-		
+		return false;
 	}
 
-	/**
-	 * Generates HTML table
-	 *
-	 * @todo Remove usage of deprecated webhelper_errorMsg()
-	 * @param string|array $data
-	 *        if tableType is 'array' $data must be an array or arrays (rows=>columns)
-	 *        if tableType is 'mysql' $data must be a SQL string
-	 * @return string
-	 */
+	public function footers($footer) {
+		if (is_string($footer)) {
+			$this->colFooters[] = $footer;
+			return true;
+		} elseif (is_array($footer)) {
+			$this->colFooters = array_merge($this->colFooters, $footer);
+			return true;
+		}
+		return false;
+	}
+
 	public function display($data) {
-		
 		$this->data = array();
-		
+
 		if ($this->tableType == "array") {
 			if (!is_array($data)) {
-				return webhelper_errorMsg("Data must be an array of arrays.");
+				return $this->errorMsg("Data must be an array of arrays.");
 			}
 			$this->data = $data;
-		}
-		else if ($this->tableType == "mysql") {
+		} elseif ($this->tableType == "mysql") {
 			if (!is_string($data)) {
-				return webhelper_errorMsg("Data must be a string (valid MySQL query).");
+				return $this->errorMsg("Data must be a string (valid MySQL query).");
 			}
-			
+
 			$data = $this->getMySQLdata($data);
-			
-			if ($data === FALSE || is_string($data)) {
+
+			if ($data === false || is_string($data)) {
 				if (is_string($data)) {
-					return webhelper_errorMsg($data);
+					return $this->errorMsg($data);
 				}
-				return webhelper_errorMsg("Error retrieving dataset.");
+				return $this->errorMsg("Error retrieving dataset.");
 			}
 		}
-		
+
 		$output = "";
-		
-		if (isnull($this->summary) && $this->summaryErrorMsg === TRUE) {
-			$output .= webhelper_errorMsg("Table Summary not provided");
+
+		if (is_null($this->summary) && $this->summaryErrorMsg === true) {
+			$output .= $this->errorMsg("Table Summary not provided");
 		}
-		
-		if ($this->sortable === TRUE) {
+
+		if ($this->sortable === true) {
 			global $engineVars;
 			$output .= "<script src=\"".$engineVars['sortableTables']."\" type=\"text/javascript\"></script>";
 		}
-		
+
 		$output .= "<table";
-		$output .= ($this->id)?' id="'.$this->id.'"':"";
-		
+		$output .= ($this->id) ? ' id="'.$this->id.'"' : "";
+
 		$output .= ' class="tableObject';
-		$output .= ($this->sortable === TRUE)?" sortable":"";
-		$output .= ($this->class)?' '.$this->class:"";
+		$output .= ($this->sortable === true) ? " sortable" : "";
+		$output .= ($this->class) ? ' '.$this->class : "";
 		$output .= '"';
-		
-		$output .= ($this->title)?' title="'.$this->title.'""':"";
-		$output .= ($this->summary)?' summary="'.$this->summary.'"':"";
+
+		$output .= ($this->title) ? ' title="'.$this->title.'"' : "";
+		$output .= ($this->summary) ? ' summary="'.$this->summary.'"' : "";
 		$output .= ">";
-		
-		if (!isnull($this->colHeaders)) {
-			if ($this->layout === TRUE) {
-				$output .= "<thead>";	
+
+		if (!empty($this->colHeaders)) {
+			if ($this->layout === true) {
+				$output .= "<thead>";
 			}
-			
+
 			$output .= '<tr class="headerRow">';
-			
-			if ($this->numberRows === TRUE) {
+
+			if ($this->numberRows === true) {
 				$output .= "<th></th>";
 			}
-			
-			foreach ($this->colHeaders as $I=>$V) {
-				$output .= '<th id="'.(($this->id)?$this->id:"").'_header_'.$V.'">';
-				$output .= $V;
+
+			foreach ($this->colHeaders as $header) {
+				$output .= '<th id="'.(($this->id) ? $this->id : "").'_header_'.$header.'">';
+				$output .= $header;
 				$output .= "</th>";
 			}
 			$output .= '</tr>';
-			
-			if ($this->layout === TRUE) {
-				$output .= "</thead>";	
+
+			if ($this->layout === true) {
+				$output .= "</thead>";
 			}
 		}
-		
-		if ($this->layout === TRUE) {
+
+		if ($this->layout === true) {
 			$output .= "<tfoot>";
 			$output .= '<tr>';
-			if (!isnull($this->colFooters)) {
-				if ($this->numberRows === TRUE) {
+			if (!empty($this->colFooters)) {
+				if ($this->numberRows === true) {
 					$output .= "<td></td>";
 				}
-				foreach ($this->colFooters as $I=>$V) {
-					$output .= '<td id="'.(($this->id)?$this->id:"").'_footer_'.$V.'">';
-					$output .= $V;
+				foreach ($this->colFooters as $footer) {
+					$output .= '<td id="'.(($this->id) ? $this->id : "").'_footer_'.$footer.'">';
+					$output .= $footer;
 					$output .= "</td>";
 				}
 			}
 			$output .= "</tr>";
-			$output .= "</tfoot>";	
+			$output .= "</tfoot>";
 		}
-		
-		if ($this->layout === TRUE) {
-			$output .= "<tbody>";	
+
+		if ($this->layout === true) {
+			$output .= "<tbody>";
 		}
-		
-		if ($this->groupBy !== NULL) {
+
+		if (!is_null($this->groupBy)) {
 			if (!is_integer($this->groupBy)) {
-				return webhelpder_errorMsg("groupBy value must be an integer.");
+				return $this->errorMsg("groupBy value must be an integer.");
 			}
-			$data = naturalSort($data,$this->groupBy);
+			$data = naturalSort($data, $this->groupBy);
 		}
-		
+
 		$numberRowsCount = 1;
 		$groupByRowCount = 1;
-		$prevGroupBy     = NULL;
-		foreach ($data as $I=>$row) {
+		$prevGroupBy = null;
+		foreach ($data as $row) {
 			$output .= "<tr";
-			if ($this->rowStriping === TRUE) {
-				$output .= (is_odd($numberRowsCount))?" class=\"oddrow\"":" class=\"evenrow\"";
+			if ($this->rowStriping === true) {
+				$output .= (is_odd($numberRowsCount)) ? " class=\"oddrow\"" : " class=\"evenrow\"";
 			}
 			$output .= ">";
-			
-			if ($this->numberRows === TRUE) {
+
+			if ($this->numberRows === true) {
 				$output .= '<td class="rowNumber">'.$numberRowsCount.'</td>';
 			}
-			
+
 			$colCount = 0;
-			foreach ($row as $K=>$V) {
-				
-				if (!isnull($this->groupBy) && $colCount == $this->groupBy && $V != $prevGroupBy) {
-					$stripeGroupBy = (is_odd($groupByRowCount++))?"groupBy_oddrow":"groupBy_evenrow";
+			foreach ($row as $key => $value) {
+				if (!is_null($this->groupBy) && $colCount == $this->groupBy && $value != $prevGroupBy) {
+					$stripeGroupBy = (is_odd($groupByRowCount++)) ? "groupBy_oddrow" : "groupBy_evenrow";
 				}
-				
+
 				$output .= "<td";
-				if (!isnull($this->groupBy) && $colCount == $this->groupBy) {
-					$output .= ' class="'.(($this->classTD === TRUE)?"td".$colCount." ":"").''.$stripeGroupBy.' groupByCell"';
-				}
-				else if ($this->classTD === TRUE) {
+				if (!is_null($this->groupBy) && $colCount == $this->groupBy) {
+					$output .= ' class="'.(($this->classTD === true) ? "td".$colCount." " : "").''.$stripeGroupBy.' groupByCell"';
+				} elseif ($this->classTD === true) {
 					$output .= ' class="td'.$colCount.'"';
 				}
 				$output .= ">";
-				if ($colCount++ != $this->groupBy || isnull($this->groupBy) || ($colCount != $this->groupBy && isset($this->groupBy) && $V != $prevGroupBy)) {
-					$output .= $V;
+				if ($colCount++ != $this->groupBy || is_null($this->groupBy) || ($colCount != $this->groupBy && isset($this->groupBy) && $value != $prevGroupBy)) {
+					# if value is an array then we need to read each one showing the creator name
+					if (is_array($value)) {
+						$creator = array();
+						foreach ($value as $id) {
+							// get creator of object
+							$creatorObject = objects::get($id);
+							
+							// add $creator['data']['title'] to creator array
+							$creator[] = $creatorObject['data']['title'];
+						}
+						$output .= implode(", ", $creator);
+					} else {
+						$output .= $value;
+					}
 				}
 				$output .= "</td>";
 			}
-			
+
 			$output .= "</tr>";
-			
+
 			$numberRowsCount++;
-			
-			if (!isnull($this->groupBy)) {
+
+			if (!is_null($this->groupBy)) {
 				$prevGroupBy = $row[$this->groupBy];
 			}
 		}
-		
-		if ($this->layout === TRUE) {
-			$output .= "</tbody>";	
-		}
-		else if ($this->layout === FALSE) {
+
+		if ($this->layout === true) {
+			$output .= "</tbody>";
+		} elseif ($this->layout === false) {
 			$output .= '<tr class="sortbottom">';
-			if (!isnull($this->colFooters)) {
-				if ($this->numberRows === TRUE) {
+			if (!empty($this->colFooters)) {
+				if ($this->numberRows === true) {
 					$output .= "<td></td>";
 				}
 				$colCount = 0;
-				foreach ($this->colFooters as $I=>$V) {
-					$output .= '<td id="'.(($this->id)?$this->id:"").'_footer_'.$V.'" '.(($this->classTD === TRUE)?'class="td'.$colCount.'" ':"").'>';
-					$output .= $V;
+				foreach ($this->colFooters as $footer) {
+					$output .= '<td id="'.(($this->id) ? $this->id : "").'_footer_'.$footer.'" '.(($this->classTD === true) ? 'class="td'.$colCount.'" ' : "").'>';
+					$output .= $footer;
 					$output .= "</td>";
 					$colCount++;
 				}
 			}
 			$output .= "</tr>";
 		}
-		
+
 		$output .= "</table>";
-	
-		return($output);		
+
+		return $output;
 	}
 
-
-	/**
-	 * Get MySQL data from database to build the table with
-	 *
-	 * @param string $data
-	 *        SQL query string
-	 * @return array|bool
-	 */
 	private function getMySQLdata($data) {
 		if (!is_string($data)) {
-			return FALSE;
+			return false;
 		}
 
-		$this->engine->openDB->sanitize = FALSE;
-		$sqlResult                = $this->engine->openDB->query($data);
-		
+		$this->engine->openDB->sanitize = false;
+		$sqlResult = $this->engine->openDB->query($data);
+
 		if (!$sqlResult['result']) {
-			return($sqlResult['error']);
-			return(FALSE);
+			return $sqlResult['error'];
+			return false;
 		}
-		
-		unset($data);
+
 		$data = array();
-		while ($row = mysql_fetch_array($sqlResult['result'],  MYSQL_ASSOC)) {
+		while ($row = mysql_fetch_array($sqlResult['result'], MYSQL_ASSOC)) {
 			$data[] = $row;
 		}
-		
-		return($data);
-		
+
+		return $data;
+	}
+
+	private function errorMsg($message) {
+		return "<div class=\"error\">".$message."</div>";
 	}
 }
-
-
-
-?>
