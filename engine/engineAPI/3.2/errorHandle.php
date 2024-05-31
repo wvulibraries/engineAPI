e6cf43a85f029cb7a55bd124db264fad57f02ee9
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2024-05-31 09:11:00 -0400

    set values to null in phpError if they are not passed

--- engine/engineAPI/3.2/errorHandle.php
+++ engine/engineAPI/3.2/errorHandle.php
@@ -352,7 +352,7 @@ class errorHandle
      * @param array  $errContext The active symbol table at the point the error occurred
      * @return bool
      */
-    public static function phpError($errNo, $errStr, $errFile, $errLine, $errContext) {
+    public static function phpError($errNo = null, $errStr = null, $errFile = null, $errLine = null, $errContext = null) {
         // we only care if the PHP error is being looked for
         $errorReporting = ini_get('error_reporting');
         
